bef15e9ce7275021f437ffb33c48b7b60cffccf4
Logan <logankeyes1212@gmail.com>
2019-02-13 11:30:03 -0600

    test2

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -86,7 +86,7 @@ $(document).ready(function () {
                   + "<div class='col'>"
                   + "<div class='collapse multi-collapse' id='collapse-card-" + i + "'>"
 
-                  //Here we can add in more p tags for the event url, price, etc. 
+                  //Here we can add in more p tags for the event url, price, etc
                   + "<p class='card-text'>Description: " + eventDescription + "</p>"
                   + "<p class='card-text'>Event Web Page: <a href='" + event.url + "'>" + event.url + "</a></p>"
                   + "</div>"
