f22870360d2cf00839553e0767b3125c3b23ce5c
Josh VanOverbeke <joshvanoverbeke@gmail.com>
2019-02-13 19:45:02 -0600

    map now works if new start point is input without page refresh

--- assets/javascript/selectedEvents.js
+++ assets/javascript/selectedEvents.js
@@ -17,33 +17,39 @@ $(document).ready(function () {
     let addresses = [];
 
 
-    $("#calculate-button").on("click", function (event) {
-        event.preventDefault();
+    $("body").on("click", "#calculate-button", function (event) {
+        
 
         startingPoint = $("#starting-point").val().trim();
         console.log(startingPoint);
+        addresses = [];
         //push starting point to array first
         addresses.push(startingPoint);
         //push each address into the array
         for (let j in chosenDisplay) {
             addresses.push(chosenDisplay[j].venue_address + ", " + chosenDisplay[j].city_name + ", " + chosenDisplay[j].region_abbr + ", " + chosenDisplay[j].postal_code)
         }
+        //clear the map
+        $("#map-part").empty();
+        //dynamically create map div and directions div
+        let div= $("<div>");
+        let mapDiv = div.attr("id", "map");
+        let directionDiv = $("<div>").attr("id", "route-narrative");
+        $("#map-part").append(mapDiv);
+        $("#map-part").append(directionDiv);
 
-
-        //clears current map
-        // $("#map").empty();
-
+        //stops map from disappearing
+        event.preventDefault();
         //initialize variables for mapquest/leaflet
         var map,
-            dir;
-
+        dir;
 
         map = L.map('map', {
             layers: MQ.mapLayer(),
             center: [39.995149, -102.045473],
             zoom: 9
         });
-
+        //text route directions
         dir = MQ.routing.directions()
         .on('success', function(data) {
             var legs = data.route.legs,
